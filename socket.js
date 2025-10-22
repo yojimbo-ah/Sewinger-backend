54dc5ba79c28078e8314f25fc707ac16cd3b972c
Abbad Ahmed <abbad.ahmed.gg@gmail.com>
2025-10-22 17:07:21 +0100

    fixing the socket it setup

--- socket.js
+++ socket.js
@@ -11,7 +11,7 @@ let io;
 export const initSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: 'http://localhost:5173',
+      origin: process.env.FRONTEND_URL ,
       methods: ['GET', 'POST' , 'PUT' , 'PATCH' , 'DELETE']
     }
   });
